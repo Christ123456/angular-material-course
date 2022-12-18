b6d186ad4f497ee50b5a82b40a09b5f69e83eb65
Christian Bernard Chekumo Dejon <cdejon@buhl-data.com>
2022-12-18 08:15:14 +0100

    von bis dialog implementation

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,24 +1,33 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA,  MatDialog,  MatDialogConfig,  MatDialogRef} from '@angular/material/dialog';
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
+import { Component, Inject, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
 })
 export class CourseDialogComponent implements OnInit {
+  Mydescription: string = "toto est laid";
 
-    constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public titel: string
+  ) {
+    this.Mydescription = titel;
+  }
 
+  ngOnInit() {}
 
-    }
-
-    ngOnInit() {
-
-    }
-
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
-
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,51 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "../services/courses.service";
-import {map} from "rxjs/operators";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { map } from "rxjs/operators";
+import { MatDialog } from "@angular/material/dialog";
+import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses$: Observable<Course[]>;
 
-    beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+  title: string;
 
-    advancedCourses$: Observable<Course[]>;
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {}
 
-    constructor(private coursesService: CoursesService) {
+  ngOnInit() {
+    const courses$ = this.coursesService.findAllCourses();
 
-    }
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "BEGINNER")
+      )
+    );
 
-    ngOnInit() {
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "ADVANCED")
+      )
+    );
+  }
 
-        const courses$ = this.coursesService.findAllCourses();
+  openDialog() {
+    const dialog = this.dialog.open(CourseDialogComponent, {
+      data: this.title,
+    });
 
-        this.beginnerCourses$ = courses$.pipe(
-          map(courses => courses.filter(course => course.category === 'BEGINNER') )
-        );
-
-        this.advancedCourses$ = courses$.pipe(
-            map(courses => courses.filter(course => course.category === 'ADVANCED') )
-        );
-
-    }
-
+    dialog.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
+      this.title = result;
+    });
+  }
 }
